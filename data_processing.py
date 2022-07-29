268e71873ecb9c07d864a5f92b6bd82794290a53
prajwalpmoolya <helloprajwal2000@gmail.com>
2022-07-29 18:46:54 +0530

    "1st line" exit
    done
    
    insert
    --insert--
    --INSERT--
    .git/commit_editmsg
    
    git commit -m 'cool'

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,1 @@
 print("adding all the data processing code related fraud detection")
-
-a = 5
-b = 7
-
-def sum(a,b):
-    c=a+b
-    return a
-
-print("adding a new function which is not correct")

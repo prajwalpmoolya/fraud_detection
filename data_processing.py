78e4bf8129902f92ee0dcd38dbd4791e4159f925
prajwalpmoolya <helloprajwal2000@gmail.com>
2022-07-30 09:16:05 +0530

    new model 1st line

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,20 @@
 print("adding all the data processing code related fraud detection")
+
+a = 5
+b = 6
+
+def sum(a,b):
+    c = a+b
+    return c
+print('sum function')
+
+def multiplication(a,b):
+    d = a*b
+    return b
+print('multplication function')
+
+def division(a,b):
+    e = a/b
+    return a
+
+print('division function')
